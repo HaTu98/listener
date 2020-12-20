c727637e078764c40b696358410013576db73242
TuHV <hatu98nd@gmail.com>
2020-12-21 01:05:20 +0700

    first commit

---
+++ src/main/java/com/tranning/listener/config/RabbitMQConfig.java
@@ -0,0 +1,45 @@
+package com.tranning.listener.config;
+
+import org.springframework.amqp.core.Queue;
+import com.tranning.listener.service.RabbitMQListner;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    @Bean
+    Queue queue() {
+            return new Queue("queue1", false);
+    }
+
+    @Bean
+    MessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory ) {
+        SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+        simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
+        simpleMessageListenerContainer.setQueues(queue());
+        simpleMessageListenerContainer.setMessageListener(new RabbitMQListner());
+        return simpleMessageListenerContainer;
+
+    }
+
+    /*@Bean
+    ConnectionFactory connectionFactory() {
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory("localhost");
+        cachingConnectionFactory.setUsername(username);
+        cachingConnectionFactory.setUsername(password);
+        return cachingConnectionFactory;
+    }
+
+    @Bean
+    MessageListenerContainer messageListenerContainer() {
+        SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+        simpleMessageListenerContainer.setConnectionFactory(connectionFactory());
+        simpleMessageListenerContainer.setQueues(queue());
+        simpleMessageListenerContainer.setMessageListener(new RabbitMQListner());
+        return simpleMessageListenerContainer;
+
+    }*/
+}
